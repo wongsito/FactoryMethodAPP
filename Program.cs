90145c7b15bb1912e5d90bcfe0a0764bd6766fa6
Daniel Wong <20203tn091@utez.edu.mx>
2023-06-13 19:40:29 -0600

    feat: update class Program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IAnimal animal1 = new Dog();
+        IAnimal animal2 = new Cat();
+
+        animal1.MakeSound(); // Output: Woof!
+        animal2.MakeSound(); // Output: Meow!
+    }
+}
