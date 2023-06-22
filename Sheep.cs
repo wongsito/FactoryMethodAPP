c81558e51c698cde81b766eedb1defee9d8e1f4a
Araceli Garcia <>
2023-06-21 19:49:53 -0600

    fix: update class Sheep - conflict resolved.

--- Sheep.cs
+++ Sheep.cs
@@ -4,6 +4,7 @@ class Sheep : IAnimal
 {
     public void MakeSound()
     {
+        Console.WriteLine("English: Baa! Baa! Baa!");
         Console.WriteLine("Spanish: Beee! Beee! Beee!");
     }
 }
